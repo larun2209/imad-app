d15180ad7f84f7c2865d79de1a3a1dbb5794ded5
larun2209 <30628858+larun2209@users.noreply.github.com>
2017-08-10 12:17:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@
 //};
 
 var button = document.getElementById('counter');
-button.onClick =function(){
+button.onclick =function(){
     counter=counter+1;
     var span= document.getElementById('count');
     span.innerHTML= counter.toString()
