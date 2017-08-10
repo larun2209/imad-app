a4dc621c331eb94a31b8b966af22d7158fc3f505
larun2209 <30628858+larun2209@users.noreply.github.com>
2017-08-10 16:22:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit_name/:name', function (req, res) {
+  var name = req.params.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
+
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
