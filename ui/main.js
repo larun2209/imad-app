a44a18b38c2ceb51bc7118e651b82761b6b9eb2f
larun2209 <30628858+larun2209@users.noreply.github.com>
2017-08-10 12:29:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,9 +13,18 @@
 //};
 
 var button = document.getElementById('counter');
-var counter=0;
+
 button.onclick =function(){
-    counter=counter+1;
-    var span= document.getElementById('count');
-    span.innerHTML= counter.toString();
+    var request=new XMLhttpRequest();
+    request.onreadystatechange-function(){
+        if(request.readystate=== XMLHttpRequest.DONE){
+            if (request.status ===200){
+                var counter = request.responseText;
+                var span= document.getElementById('count');
+                span.innerHTML= counter.toString();
+                                        }
+                                          }
+                            };
+   
+    
 };
