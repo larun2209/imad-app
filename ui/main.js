caf4de9099423ef3876f7b605377a101f7cf474f
larun2209 <30628858+larun2209@users.noreply.github.com>
2017-08-13 15:00:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,58 +1,14 @@
-//console.log('Loaded!');
-//var element = document.getElementById('main-text');
-//element.innerHTML='New value';
-//var img = document.getElementById('madi');
-//var marginLeft=0;
-//function moveRight(){
-  //  marginLeft=marginLeft+1;
-    //img.style.marginLeft=marginLeft+'px';
-//}
-//img.onclick=function(){
-  //  var interval= setInterval(moveRight,50);
-    //img.style.marginLeft ='100px';
-//};
 
-var button = document.getElementById('counter');
 
-button.onclick =function(){
-    
-    var request= new XMLHttpRequest();
-    
-    request.onreadystatechange-function(){
-        if(request.readystate=== XMLHttpRequest.DONE){
-            if (request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-                                        }
-                                          }
-                            };
-   
-   request.open('GET','http://larun22091974.imad.hasura-app.io/counter',true);
-   request.send(null);
-};
 
-var nameInput= document.getElementById('name');
-var name = nameInput.value;
-var submit= document.getElementById('submit_btn');
-submit.onclick= function(){
-    var names=['nn1','nn2','nn3'];
-    var list='';
-    for(var i=0; i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-   var ul= document.getElementById('namelist');
-    ul.innerHTML=list;
-};
-
-var commentsInput= document.getElementById('comments');
-var comments = commentsInput.value;
-var enter= document.getElementById('enter_btn');
-enter.onclick= function(){
-    var commentlist=[];
-    for(var i=0; i<comments.length;i++){
-        commentlist+='<li>'+comments[i]+'</li>';
-    }
-   var ul= document.getElementById('commentlist');
-    ul.innerHTML=list;
-};
+        var i=0;
+        var list=[];
+        var enter= document.getElementById('enter_btn');
+        enter.onclick= function(){
+                var Input= document.getElementById('comments');
+                var comments = Input.value;
+                list[i]='<li>'+ comments + '</li>';
+                var ul = document.getElementById('commentlist');
+                ul.innerHTML= list;
+                i++;
+            };
