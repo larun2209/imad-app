14da948a9ad9f8b29024402fa49a7693b620d1c6
larun2209 <30628858+larun2209@users.noreply.github.com>
2017-08-13 16:59:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,12 +2,12 @@
 
 
         var i=0;
-        var list=[];
+        var list='';
         var enter= document.getElementById('enter_btn');
         enter.onclick= function(){
                 var Input= document.getElementById('comments');
                 var comments = Input.value;
-                list[i]='<li>'+ comments + '</li>';
+                list+='<li>'+ comments + '</li>';
                 var ul = document.getElementById('commentlist');
                 ul.innerHTML= list;
                 i++;
