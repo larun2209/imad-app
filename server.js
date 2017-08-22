da567f0ab8718c140632e39f17e3b57da2c5503e
larun2209 <30628858+larun2209@users.noreply.github.com>
2017-08-22 11:50:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config={
     user:'larun22091974',
@@ -23,6 +24,16 @@ app.get('/counter', function (req, res) {
     counter=counter+1;
     res.send(counter.toString());
 });
+
+function hash(input,salt){
+    var hashed= crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return(hashed.toString(hex));
+}
+app.get('/hash/:input', function (req, res){
+    var hashedString = hash(req.params.input,"this is some random string");
+    res.send(hashedString);
+});
+
 var pool= new Pool(config);
 app.get('/test-db', function (req, res) {
     pool.query('SELECT * from test',function(err,result){
