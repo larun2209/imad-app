53a52c477fe7cb0558e844665018b01e3911fe12
larun2209 <30628858+larun2209@users.noreply.github.com>
2017-08-24 20:11:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,9 +28,9 @@
                     }
                 };
                 
-                request.open('POST','http://larun22091974.imad.hasura-app.io/login',true);
-                request.setRequestHeader("Content-Type","application/json");
-                request.send(JSON.stringify({username:username,password:password}));
+               // request.open('POST','http://larun22091974.imad.hasura-app.io/login',true);
+               // request.setRequestHeader("Content-Type","application/json");
+               // request.send(JSON.stringify({username:username,password:password}));
               
              
             };    
