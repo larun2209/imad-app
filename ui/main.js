1f7dee7526ec9c4679caf2248c07b528dd3bf0fa
larun2209 <30628858+larun2209@users.noreply.github.com>
2017-08-24 20:21:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,31 +41,31 @@
         
         
         
-        var list='';
-        var enter= document.getElementById('enter_btn');
-        enter.onclick= function(){
-                var Input= document.getElementById('comments');
-                var comments = Input.value;
-                list+='<li>'+ comments + '</li>';
-                var ul = document.getElementById('commentlist');
-                ul.innerHTML= list;
+       // var list='';
+       // var enter= document.getElementById('enter_btn');
+       // enter.onclick= function(){
+       //         var Input= document.getElementById('comments');
+       //         var comments = Input.value;
+        //        list+='<li>'+ comments + '</li>';
+        //        var ul = document.getElementById('commentlist');
+        //        ul.innerHTML= list;
                 
                 
-                var request = new XMLHttpRequest();
-                request.onreadystatechange = function()
-                {
-                    if(request.readyState === XMLHttpRequest.DONE)
-                    {
-                        if (request.status=== 200)
-                        {
-                            var counterrxd= request.responseText;
-                            var span= document.getElementById('counter'); 
-                            span.innerHTML= counterrxd.toString();
-                        }
-                    }
-                };
-                request.open('GET','http://larun22091974.imad.hasura-app.io/counter',true);
-                request.send(null);
+         //       var request = new XMLHttpRequest();
+         //       request.onreadystatechange = function()
+         //       {
+         //           if(request.readyState === XMLHttpRequest.DONE)
+         //           {
+          //              if (request.status=== 200)
+          //              {
+          //                  var counterrxd= request.responseText;
+           //                 var span= document.getElementById('counter'); 
+           //                 span.innerHTML= counterrxd.toString();
+           //             }
+           //         }
+            //    };
+            //    request.open('GET','http://larun22091974.imad.hasura-app.io/counter',true);
+            //    request.send(null);
               
              
-            };
+           // };
