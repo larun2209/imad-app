d25f9d61299ab60f8588c757ff1b6ad1fc7dd58b
larun2209 <30628858+larun2209@users.noreply.github.com>
2017-08-24 15:43:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,18 +6,28 @@
                 console.log(pwd);
                 
                 var request = new XMLHttpRequest();
+                
                 request.onreadystatechange = function()
                 {
                     if(request.readyState === XMLHttpRequest.DONE)
                     {
                         if (request.status=== 200)
                         {
-                            var counterrxd= request.responseText;
-                            var span= document.getElementById('counter'); 
-                            span.innerHTML= counterrxd.toString();
+                            alert("user logged in successfully");
+                        }
+                        else if (request.status=== 403) 
+                        {
+                            
+                            alert("user name/password is incorrect");
+                        }
+                         else if (request.status=== 500) 
+                        {
+                            
+                            alert("something went wrong on the server");
                         }
                     }
                 };
+                
                 request.open('GET','http://larun22091974.imad.hasura-app.io/counter',true);
                 request.send(null);
               
